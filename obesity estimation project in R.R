d365c42411052376446137a83c2022fddd7afde5
codeboxxx <clintonsilvanus@gmail.com>
2023-09-06 13:11:43 +0100

    obesity project

--- obesity estimation project in R.R
+++ obesity estimation project in R.R
@@ -12,3 +12,17 @@ View(ObesityFile)
 library("dplyr")
 library("ggplot2")
 
+View(ObesityFile$Value)
+
+RoundValue <- round(ObesityFile$Value, digits = 2)
+RoundValue
+
+percValue <- ObesityFile$Value * 100
+percValue
+
+ObesityFile2 <- mutate(ObesityFile, percValue)
+ObesityFile2
+View(ObesityFile2)
+
+totalYear <- ObesityFile2 %>% group_by(`Age Group`)
+totalYear
