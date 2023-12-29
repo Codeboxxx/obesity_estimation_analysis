1c4379f234c200101b2bd9ea8d39885019f8283d
codeboxxx <clintonsilvanus@gmail.com>
2023-12-29 16:13:47 +0100

    Cleaner codes, analyzing the obesity data and seeing how the different age groups corelate with the different years

--- obesity estimation project in R.R
+++ obesity estimation project in R.R
@@ -1,7 +1,14 @@
-install.packages("readxl")
 library("readxl")
+library("dplyr")
+library("ggplot2")
+library("tidyverse")
+
+
+#reading the excel file
 ObesityFile <- read_excel("C:/Users/user/Documents/R projects/obesity-estimation-by-age-group-and-year.xlsx", col_names = TRUE, na= "")
 ObesityFile
+
+#observing the file
 str(ObesityFile)
 
 head(ObesityFile, 10)
@@ -9,25 +16,44 @@ tail(ObesityFile, 10)
 
 View(ObesityFile)
 
-library("dplyr")
-library("ggplot2")
-library("tidyverse")
+
 
 View(ObesityFile$Value)
 
-RoundValue <- round(ObesityFile$Value, digits = 2)
-RoundValue
-
+#rounding up the value column by multiplying the values by 100 and creating a new column
 percValue <- ObesityFile$Value * 100
 percValue
 
+#adding the new column to the data
 ObesityFile2 <- mutate(ObesityFile, percValue)
 ObesityFile2
-View(ObesityFile2)
 
 
 ObesityFile2 <- ObesityFile2 %>% 
-  filter(`Age Group` != "TOTAL")
+  filter(`Age Group`!= "TOTAL")
 
 View(ObesityFile2)
+
+
+#sub-setting the data into groups to see each values from all years for each age group
+teenGroups <- ObesityFile2 %>% 
+  filter(`Age Group`== "18-29 years")
+view(teenGroups)
+
+middlegroup <- ObesityFile2 %>% 
+  filter(`Age Group`== "30-44 years")
+
+adultgroup <- ObesityFile2 %>% 
+  filter(`Age Group`== "45-59 years")
+
+senioradultgroup <- ObesityFile2 %>% 
+  filter(`Age Group`== "60+ years")
+
+view(teenGroups)
+view(adultgroup)
+view(middlegroup)
+view(senioradultgroup)
+
+teen_avg <- ObesityFile2 %>% 
+  group_by()
 
